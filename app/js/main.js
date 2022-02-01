adc91f63e23e9f520b15fad9765e248fab4a5d36
datoshcode <datoshcode@gmail.com>
2022-02-01 21:44:03 +0200

    added promo and partners

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 $(function() {
   
+  // Слайдер
   $('.top-slider__inner').slick({
    dots: true,
    arrows: false,
@@ -8,6 +9,7 @@ $(function() {
    autoplaySpead: 2000
   });
 
+// Звездный рейтинг
   $(".star").rateYo({
     rating: 3.6,
     starWidth: "17px",
@@ -15,5 +17,49 @@ $(function() {
     ratedFill: "#ffc35b",
     readOnly: true 
   });
+
+// Счетчик отсчета времени
+  function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  const clock = document.querySelector('.promo__clock');
+  const daysSpan = clock.querySelector('.promo__days');
+  const hoursSpan = clock.querySelector('.promo__hours');
+  const minutesSpan = clock.querySelector('.promo__minutes');
+  const secondsSpan = clock.querySelector('.promo__seconds');
+
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
+
+const deadline = $('.promo__clock').attr('data-time');
+initializeClock('promo__clock', deadline);
 
 });
