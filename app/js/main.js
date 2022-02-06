40c88997e770fc4d3a0690f54cd47ec21622c76b
datoshcode <datoshcode@gmail.com>
2022-02-06 22:31:49 +0200

    added filter-price and filter-color in shop page

--- app/js/main.js
+++ app/js/main.js
@@ -3,69 +3,77 @@ $(function() {
   // Фильтер стоимости товара
   $('.filter-price__input').ionRangeSlider({
     type: "double",
-    prefix: "$"
-  });
-  
-  // Слайдер
-  $('.top-slider__inner').slick({
-   dots: true,
-   arrows: false,
-   fade: true,
-   autoplay: true,
-   autoplaySpead: 2000
+    prefix: "$",
+     onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+     },
+    onChange: function(data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
   });
 
-// Звездный рейтинг
+  // Слайдер
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpead: 2000
+  });
+
+  // Звездный рейтинг
   $(".star").rateYo({
     rating: 3.6,
     starWidth: "17px",
     normalFill: "#ccccce",
     ratedFill: "#ffc35b",
-    readOnly: true 
+    readOnly: true
   });
 
-// Счетчик отсчета времени
+  // Счетчик отсчета времени
   function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
-  const seconds = Math.floor((total / 1000) % 60);
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(total / (1000 * 60 * 60 * 24));
-  
-  return {
-    total,
-    days,
-    hours,
-    minutes,
-    seconds
-  };
-}
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
 
-function initializeClock(id, endtime) {
-  const clock = document.querySelector('.promo__clock');
-  const daysSpan = clock.querySelector('.promo__days');
-  const hoursSpan = clock.querySelector('.promo__hours');
-  const minutesSpan = clock.querySelector('.promo__minutes');
-  const secondsSpan = clock.querySelector('.promo__seconds');
-
-  function updateClock() {
-    const t = getTimeRemaining(endtime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
   }
 
-  updateClock();
-  const timeinterval = setInterval(updateClock, 1000);
-}
+  function initializeClock(id, endtime) {
+    const clock = document.querySelector('.promo__clock');
+    const daysSpan = clock.querySelector('.promo__days');
+    const hoursSpan = clock.querySelector('.promo__hours');
+    const minutesSpan = clock.querySelector('.promo__minutes');
+    const secondsSpan = clock.querySelector('.promo__seconds');
 
-const deadline = $('.promo__clock').attr('data-time');
-initializeClock('promo__clock', deadline);
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  const deadline = $('.promo__clock').attr('data-time');
+  initializeClock('promo__clock', deadline);
 
 });
