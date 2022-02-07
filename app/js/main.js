ef6c065a236dc54afecfac36495b133913bffb74
datoshcode <datoshcode@gmail.com>
2022-02-07 23:08:56 +0200

    added product item in shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,30 @@
 $(function() {
 
+  // Сортировка на плитки или список товара
+  $('.shop-content__filter-btn').on('click', function() {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function(){
+   $('.product-item').addClass('product-item--list');
+  });
+
+  $('.button-grid').on('click', function(){
+   $('.product-item').removeClass('product-item--list');
+  });
+
+  // Стилизация форм
+  $('.select-style').styler();
+
   // Фильтер стоимости товара
   $('.filter-price__input').ionRangeSlider({
     type: "double",
     prefix: "$",
-     onStart: function (data) {
+    onStart: function(data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
-     },
+    },
     onChange: function(data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
